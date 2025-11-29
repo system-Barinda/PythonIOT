198aaa2e7a1701975c399489f646f04e4edda133
system sylvere BARINDA <systembarinda@gmail.com>
2025-11-29 21:50:46 +0200

    done

--- src/core/config.py
+++ src/core/config.py
@@ -1,1 +1,9 @@
-DB_URL = "postgresql://neondb_owner:npg_fSZHvgry24wA@ep-young-heart-a4261tls-pooler.us-east-1.aws.neon.tech/barinda_python?sslmode=require&channel_binding=require"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:12345@localhost:5432/python_iot"
+)
