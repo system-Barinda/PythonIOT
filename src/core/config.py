b14ddb43fe12e9b7dc12542eb86417d9b00d7b11
system sylvere BARINDA <systembarinda@gmail.com>
2025-11-29 22:23:12 +0200

    deon

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# IMPORTANT: explicitly load the .env inside /src
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(os.path.dirname(BASE_DIR), ".env")
+
+load_dotenv(ENV_PATH)
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+asyncpg://postgres:12345@localhost:5432/python_iot"
+    "postgresql://postgres:12345@localhost:5432/python_iot"
 )
